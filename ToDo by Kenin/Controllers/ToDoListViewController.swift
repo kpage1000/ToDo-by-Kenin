3f5e9145fb6d1c1012a02210ecd0ab928ab408ea
Kenin Page <kenin.page@kenins-macbook-pro.local>
2018-02-07 11:39:01 -0500

    post coddle save cleanup, eliminated comments.

--- ToDo by Kenin/Controllers/ToDoListViewController.swift
+++ ToDo by Kenin/Controllers/ToDoListViewController.swift
@@ -12,9 +12,6 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
-    //used to save data to UserDefault data
-    //let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,18 +36,11 @@ class ToDoListViewController: UITableViewController {
         //value = condition ? valueIfTrue : valueIfFalse
         cell.accessoryType = item.done ? .checkmark : .none
         
-        //replaces the below lines
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
         return cell
     }
     
     //MARK - Create TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
@@ -63,18 +53,12 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when the user clicks the add button on our UIAlert
-            
-            //TODO need to validate data first
             if textField.text != "" {
                 
                 let newItem = Item()
                 newItem.title = textField.text!
                 
                 self.itemArray.append(newItem)
-                
-                //TODO KENIN the next call fails, why? defaultUser is only for small data sets?
-                //this is how we originally save data to the userDefault data area, used for small config data
-                //self.defaults.set(self.itemArray, forKey: "ToDoListArray")
                 
                 self.saveItems()
                 
